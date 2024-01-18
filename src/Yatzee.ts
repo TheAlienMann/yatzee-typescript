e1784c64c0245d49f4dba3c0031fcc2bb9ec6373
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 01:20:33 +0330

    refactor `chance` to use HOFs instead.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -10,14 +10,15 @@ export default class Yatzee {
         this.dice[4] = _5;
     }
 
-    static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    static chance(...args: number[]): number {
+        // var total = 0;
+        // total += d1;
+        // total += d2;
+        // total += d3;
+        // total += d4;
+        // total += d5;
+        // return total;
+        return args.reduce((acc, val) => acc + val, 0);
     }
 
     static yatzy(...args: number[]): number {
