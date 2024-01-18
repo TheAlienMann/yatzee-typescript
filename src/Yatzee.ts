66a554c7d5ba920b069d2832d4fcc90e7da92f47
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 01:27:07 +0330

    working

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -30,13 +30,6 @@ export default class Yatzee {
 
     static twos(...args: number[]): number {
         return args.filter((num) => num == 2).reduce((acc, val) => acc + val, 0);
-        // var sum = 0;
-        // if (d1 == 2) sum += 2;
-        // if (d2 == 2) sum += 2;
-        // if (d3 == 2) sum += 2;
-        // if (d4 == 2) sum += 2;
-        // if (d5 == 2) sum += 2;
-        // return sum;
     }
 
     static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
