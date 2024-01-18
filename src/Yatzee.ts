ec742b286df924b9912a518d1edd6dd67b86dedd
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 01:22:49 +0330

    refactor `ones` to use HOFs instead.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -24,15 +24,16 @@ export default class Yatzee {
         return 0;
     }
 
-    static ones(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) sum++;
-
-        return sum;
+    static ones(...args: number[]): number {
+        return args.filter((num) => num == 1).reduce((acc, val) => acc + val, 0);
+        // var sum = 0;
+        // if (d1 == 1) sum++;
+        // if (d2 == 1) sum++;
+        // if (d3 == 1) sum++;
+        // if (d4 == 1) sum++;
+        // if (d5 == 1) sum++;
+        //
+        // return sum;
     }
 
     static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
