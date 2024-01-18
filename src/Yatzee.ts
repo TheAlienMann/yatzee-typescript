08e96b15ca61ac7ffa650e504e2b50400ecb4fc9
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 01:55:26 +0330

    use `let` instaed of `var`.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -17,7 +17,7 @@ export default class Yatzee {
     static yatzy(...args: number[]): number {
         let counts = [0, 0, 0, 0, 0, 0, 0, 0];
         for (var i = 0; i != args.length; ++i) {
-            var die = args[i];
+            let die = args[i];
             counts[die - 1]++;
         }
         for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
