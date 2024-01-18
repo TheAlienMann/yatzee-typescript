0ddb861ea551716e036e4cc459a5abe87035bdef
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 02:02:32 +0330

    use `let` instaed of `var`.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -48,14 +48,14 @@ export default class Yatzee {
     }
 
     static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         counts[d1 - 1]++;
         counts[d2 - 1]++;
         counts[d3 - 1]++;
         counts[d4 - 1]++;
         counts[d5 - 1]++;
-        var n = 0;
-        var score = 0;
+        let n = 0;
+        let score = 0;
         for (let i = 0; i < 6; i += 1)
             if (counts[6 - i - 1] >= 2) {
                 n++;
