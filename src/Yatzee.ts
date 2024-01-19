c21ee8607cedc90074e50eeb060ec0d68b523c2f
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 13:56:39 +0330

    refactor - introduce args array instead of multiple inputs.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -99,13 +99,13 @@ export default class Yatzee {
         return 0;
     }
 
-    static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    static smallStraight(...args: number[]): number {
         let tallies = [0, 0, 0, 0, 0, 0, 0];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
+        tallies[args[1 - 1] - 1] += 1;
+        tallies[args[2 - 1] - 1] += 1;
+        tallies[args[3 - 1] - 1] += 1;
+        tallies[args[4 - 1] - 1] += 1;
+        tallies[args[5 - 1] - 1] += 1;
         if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
         return 0;
     }
