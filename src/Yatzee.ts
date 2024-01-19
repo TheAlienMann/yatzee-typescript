1625c018da0a6326511f04bc1ac1364eec92b45b
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 14:25:52 +0330

    r -

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -111,7 +111,7 @@ export default class Yatzee {
     }
 
     static largeStraight(...args: number[]): number {
-        let tallies = [0, 0, 0, 0, 0, 0, 0, 0];
+        let tallies = [0, 0, 0, 0, 0, 0];
         tallies[args[1 - 1] - 1] += 1;
         tallies[args[2 - 1] - 1] += 1;
         tallies[args[3 - 1] - 1] += 1;
