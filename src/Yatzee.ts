07418eb5bb371f5d01cde08a864b2f491963a270
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-19 13:50:59 +0330

    join variable declaration in `four_of_a_kind`.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -78,8 +78,7 @@ export default class Yatzee {
     }
 
     static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
-        var tallies;
-        tallies = [0, 0, 0, 0, 0, 0, 0, 0];
+        let tallies = [0, 0, 0, 0, 0, 0, 0, 0];
         tallies[_1 - 1]++;
         tallies[_2 - 1]++;
         tallies[d3 - 1]++;
