e1b007cb1fbdf99cca4cfffde40c335222bc63b3
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-20 19:30:14 +0330

    clean up.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -108,8 +108,6 @@ export default class Yatzee {
         tallies[args[5 - 1] - 1] += 1;
         const everySingleItemIsOne = (value) => value == 1;
         return tallies.every(everySingleItemIsOne) ? 15 : 0;
-        // if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
-        // return 0;
     }
 
     static largeStraight(...args: number[]): number {
