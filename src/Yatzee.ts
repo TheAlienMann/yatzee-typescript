71bcd9df3f6ccecabbc557f6bad0ed5ec89305c4
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-20 19:03:51 +0330

    r - get rid of unnecessary extra items in counts array, `four_of_a_kind` method.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -78,13 +78,13 @@ export default class Yatzee {
     }
 
     static four_of_a_kind(...args: number[]): number {
-        let tallies = [0, 0, 0, 0, 0, 0, 0];
+        let tallies = [0, 0, 0, 0, 0];
         tallies[args[1 - 1] - 1]++;
         tallies[args[2 - 1] - 1]++;
         tallies[args[3 - 1] - 1]++;
         tallies[args[4 - 1] - 1]++;
         tallies[args[5 - 1] - 1]++;
-        for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
+        for (let i = 0; i < 5; i++) if (tallies[i] >= 4) return (i + 1) * 4;
         return 0;
     }
 
