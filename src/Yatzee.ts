e81de6588e855632d75a47d79333d5a7c3cba5fe
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-21 18:34:33 +0330

    r - shorten the `yatzee` method with creating a Set out of the given array input.

--- src/Yatzee.ts
+++ src/Yatzee.ts
@@ -15,13 +15,14 @@ export default class Yatzee {
     }
 
     static yatzy(...args: number[]): number {
-        let counts = [0, 0, 0, 0, 0, 0, 0, 0];
-        for (let i = 0; i != args.length; ++i) {
-            let die = args[i];
-            counts[die - 1]++;
-        }
-        for (let i = 0; i != 6; i++) if (counts[i] == 5) return 50;
-        return 0;
+        return new Set(args).size == 1 ? 50 : 0;
+        // let counts = [0, 0, 0, 0, 0, 0, 0, 0];
+        // for (let i = 0; i != args.length; ++i) {
+        //     let die = args[i];
+        //     counts[die - 1]++;
+        // }
+        // for (let i = 0; i != 6; i++) if (counts[i] == 5) return 50;
+        // return 0;
     }
 
     static ones(...args: number[]): number {
